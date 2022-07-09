bfff88f3a5b79be8f610e25a3c28f2c4be497fd7
Vildan <vildan84@mail.ru>
2022-07-09 11:26:56 +0300

    Second task ready

---
+++ Task02/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter first number: ");
+int number_one = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int number_two = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter third number: ");
+int number_three = Convert.ToInt32(Console.ReadLine());
+
+int max_number = number_one;
+
+if(number_two > max_number)
+{
+    max_number = number_two ;
+}
+else if(number_three > max_number)
+{
+    max_number = number_three;
+}
+Console.WriteLine("Max number is: " + max_number);
