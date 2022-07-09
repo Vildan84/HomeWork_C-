7f347d525b11378984a5fda727a90356c9ba4017
Vildan <vildan84@mail.ru>
2022-07-09 11:41:24 +0300

    Third task ready

---
+++ Task03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.WriteLine("Even number");
+}
+else
+{
+    Console.WriteLine("Odd number");
+}
