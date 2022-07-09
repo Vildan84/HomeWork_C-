78fe2cd7fcf08cd1cde711a496477d5af06003dd
Vildan <vildan84@mail.ru>
2022-07-09 15:38:05 +0300

    Fourth task ready

---
+++ Task04/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int tag = 1;
+
+while(tag <= number)
+{
+    if(tag % 2 == 0)
+    {
+        Console.WriteLine(tag);
+    }
+    tag += 1;
+}
